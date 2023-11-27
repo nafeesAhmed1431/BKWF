fa231125fdfb01d43dafe1381304791c538487ae
Asad Ansari <asadansari1319@gmail.com>
2023-11-27 16:04:49 +0500

    Update footer.php

--- themes/default/admin/views/footer.php
+++ themes/default/admin/views/footer.php
@@ -105,6 +105,38 @@ $s2_file_date = $this->parser->parse_string($s2_lang_file, $s2_data, true);
                 $('#sidebar').toggleClass('sicebarActive');
             }
         });
+
+
+        $('.userContentScreen').on('click', function() {
+            toggleFullScreen();
+        });
+
+        function toggleFullScreen() {
+            if (!document.fullscreenElement &&
+                !document.mozFullScreenElement &&
+                !document.webkitFullscreenElement &&
+                !document.msFullscreenElement) {
+                if (document.documentElement.requestFullscreen) {
+                    document.documentElement.requestFullscreen();
+                } else if (document.documentElement.mozRequestFullScreen) {
+                    document.documentElement.mozRequestFullScreen();
+                } else if (document.documentElement.webkitRequestFullscreen) {
+                    document.documentElement.webkitRequestFullscreen();
+                } else if (document.documentElement.msRequestFullscreen) {
+                    document.documentElement.msRequestFullscreen();
+                }
+            } else {
+                if (document.exitFullscreen) {
+                    document.exitFullscreen();
+                } else if (document.mozCancelFullScreen) {
+                    document.mozCancelFullScreen();
+                } else if (document.webkitExitFullscreen) {
+                    document.webkitExitFullscreen();
+                } else if (document.msExitFullscreen) {
+                    document.msExitFullscreen();
+                }
+            }
+        }
     });
 </script>
 
